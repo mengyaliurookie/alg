28dafa3c6b5d3a951ba7cfdc8a630d04916673ca
lmy <mengyaliu6013@163.com>
2025-08-06 12:27:13 +0800

    day1 刷题 完毕

--- 704.py
+++ 704.py
@@ -34,3 +34,65 @@ class Solution:
                 res=mid
                 break
         return res
+
+
+# 27 移除元素
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # 使用双指针，找到开头第一个等于target的元素位置，同时找到结尾第一个不等于target的元素
+        # 然后交换元素，直到遍历完所有元素
+        target=val
+        fir=0
+        sec=len(nums)-1
+        while fir<=sec:
+            while fir<=sec and nums[fir]!=target:
+                fir+=1
+            # 此时fir指向的元素一定等于target
+            while fir<=sec and nums[sec]==target:
+                sec-=1
+            if fir<=sec:
+                # 交换位置
+                nums[fir],nums[sec]=nums[sec],nums[fir]
+                # 此时fir位置处一定不为target
+                fir+=1
+                # 此时sec位置一定为target
+                sec-=1
+        k=len(nums)
+        for i,v in enumerate(nums):
+            if v==target:
+                k=i
+                break
+        return k
+
+# 977 有序数组的平方
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # 因为有负数，所以需要找到正负数交汇的地方，然后向两边遍历
+        # 很像归并排序中的合并阶段
+        # 先找到第一个正数
+        fir = len(nums)
+        for i, v in enumerate(nums):
+            if v > 0:
+                fir = i
+                break
+        # 然后找到负数的位置
+        sec = fir - 1
+        ans = []
+        while sec > -1 and fir < len(nums):
+            if nums[sec] * nums[sec] > nums[fir] * nums[fir]:
+                ans.append(nums[fir] * nums[fir])
+                fir += 1
+            else:
+                ans.append(nums[sec] * nums[sec])
+                sec -= 1
+        while sec > -1:
+            ans.append(nums[sec] * nums[sec])
+            sec -= 1
+        while fir < len(nums):
+            ans.append(nums[fir] * nums[fir])
+            fir += 1
+        return ans
+
+
+
+
