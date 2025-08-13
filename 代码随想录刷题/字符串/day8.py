839acfd7c1462d4727f9d202b97a51d26676de26
lmy <mengyaliu6013@163.com>
2025-08-13 18:28:28 +0800

    day8 字符串

---
+++ 代码随想录刷题/字符串/day8.py
@@ -0,0 +1,60 @@
+
+
+# 344.反转字符串
+topic="""
+    编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+ 
+
+示例 1：
+
+输入：s = ["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # 使用双指针的方式交换收尾元素
+        fir=0
+        sec=len(s)-1
+        while fir<sec:
+            s[fir],s[sec]=s[sec],s[fir]
+            fir+=1
+            sec-=1
+
+
+# 541. 反转字符串II
+topic="""
+    给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ 
+
+示例 1：
+
+输入：s = "abcdefg", k = 2
+输出："bacdfeg"
+"""
+
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        n = len(s)
+        sl = [i for i in s]
+
+        for start in range(0, n, k):
+            # print('start: ',start)
+            if (start / k) % 2 == 0:
+                fir = start
+                sec = min(start + k - 1, n - 1)
+                while fir < sec:
+                    sl[fir], sl[sec] = sl[sec], sl[fir]
+                    fir += 1
+                    sec -= 1
+        return "".join(sl)
