5ea4022cb027cf190f11a7423d95ae34313bd6f1
mengyaliu <mengyaliu6013@163.com>
2025-08-16 22:54:39 +0800

    day10 栈和队列

--- 代码随想录刷题/栈和队列/day11.py
+++ 代码随想录刷题/栈和队列/day11.py
@@ -81,3 +81,90 @@ topic="""
 """
 
 
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        stack = Mystack()
+        ans = []
+        for ind, val in enumerate(nums):
+            if ind - k + 1 < 0:
+                stack.push(val)
+                continue
+            # 先入栈
+            stack.push(val)
+            ans.append(stack.peak())
+            # 弹出栈窗口左边的元素
+            stack.pop(nums[ind - k + 1])
+            # print(stack)
+        return ans
+
+
+class Mystack:
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, val):
+        if not self.stack:
+            self.stack.append(val)
+        else:
+            while self.stack:
+                if self.stack[0] < val:
+                    self.stack.popleft()
+                else:
+                    break
+            self.stack.appendleft(val)
+
+    def pop(self, val):
+        if self.stack[-1] == val:
+            self.stack.pop()
+
+    def peak(self):
+        return self.stack[-1]
+
+    def __str__(self):
+        return f"Stack: {list(self.stack)}"
+
+# 347.前 K 个高频元素
+topic="""
+    给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+ 
+
+示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+
+输入: nums = [1], k = 1
+输出: [1]
+ 
+
+提示：
+
+1 <= nums.length <= 105
+k 的取值范围是 [1, 数组中不相同的元素的个数]
+题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+ 
+
+进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
+
+"""
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = defaultdict(int)
+        for i in nums:
+            d[i] += 1
+        dr = defaultdict(list)
+        for i, v in d.items():
+            dr[v].append(i)
+        ans = []
+        vl = list(dr.keys())
+        vl.sort(reverse=True)
+        for v in vl:
+            ans.extend(dr[v])
+            if len(ans) >= k:
+                break
+        return ans[:k]
+
