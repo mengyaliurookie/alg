18a22972be31908caaa14167a95e0e9ee3d731f1
lmy <mengyaliu6013@163.com>
2025-08-29 18:26:15 +0800

    day24 n皇后问题

--- 代码随想录刷题/回溯/day24.py
+++ 代码随想录刷题/回溯/day24.py
@@ -136,3 +136,185 @@ class Solution:
             queue.appendleft(key)
         dfs('JFK')
         return list(queue)
+
+
+# 51. N皇后
+topic="""
+    按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+
+n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+
+每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位
+
+输入：n = 4
+输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+解释：如上图所示，4 皇后问题存在两个不同的解法。
+示例 2：
+
+输入：n = 1
+输出：[["Q"]]
+"""
+
+# 这基本是自己完成的思路，但是代码写的比较差，然后中间借助ai调试了很多次边界问题。
+# 还有一个重要的错误就是，之前是用True和False来表示是否访问过，但是这样会出错，因为回溯的时候会错误的取消掉上层的访问状态。因为同一个点会被不同的皇后访问，所以需要用计数的方式来表示访问过
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # 尝试遍历第一个皇后可以选择的位置1-n，当选定之后，会把地图划分为很多区域（区域必须是连通的）
+        # 当区域大小退化为1或者2,3,4的时候，那么这个区域只能放一个
+        # 当选定一个位置之后，把其他位置全部标记为不可访问，
+        # 每行必须有一个，每列必须有一个。不然必会出现两个在同一行或者同一列的问题，这必然会导致相互攻击的问题
+        # 可以先看看当一行选定之后，对下一行的影响
+        dp = [[0] * n for _ in range(n)]
+        ans = []
+        path = []
+
+        def tag(row, col):
+            # 把行列标记为不可访问
+            for i in range(n):
+                dp[row][i] += 1
+                dp[i][col] += 1
+            # 把斜着的标记为不可访问
+            # row加col加，row加col减
+            # row减col加，row减col减
+            # 四种情况
+            # row减col加
+            rowj = row
+            colj = col
+            while rowj >= 0 and colj < n:
+                dp[rowj][colj] += 1
+                rowj -= 1
+                colj += 1
+
+            # row减col减
+            rowj = row
+            colm = col
+            while rowj >= 0 and colm >= 0:
+                dp[rowj][colm] += 1
+                rowj -= 1
+                colm -= 1
+
+            # row加col减
+            rowj = row
+            colm = col
+            while rowj < n and colm >= 0:
+                dp[rowj][colm] += 1
+                rowj += 1
+                colm -= 1
+
+            # row加col加
+            rowj = row
+            colm = col
+            while rowj < n and colm < n:
+                dp[rowj][colm] += 1
+                rowj += 1
+                colm += 1
+
+        def untag(row, col):
+            # 把行列标记为不可访问
+            for i in range(n):
+                dp[row][i] -= 1
+                dp[i][col] -= 1
+            # 把斜着的标记为不可访问
+            # row加col加，row加col减
+            # row减col加，row减col减
+            # 四种情况
+            # row减col加
+            rowj = row
+            colj = col
+            while rowj >= 0 and colj < n:
+                dp[rowj][colj] -= 1
+                rowj -= 1
+                colj += 1
+
+            # row减col减
+            rowj = row
+            colm = col
+            while rowj >= 0 and colm >= 0:
+                dp[rowj][colm] -= 1
+                rowj -= 1
+                colm -= 1
+
+            # row加col减
+            rowj = row
+            colm = col
+            while rowj < n and colm >= 0:
+                dp[rowj][colm] -= 1
+                rowj += 1
+                colm -= 1
+
+            # row加col加
+            rowj = row
+            colm = col
+            while rowj < n and colm < n:
+                dp[rowj][colm] -= 1
+                rowj += 1
+                colm += 1
+
+        def dfs(row):
+            if row == n:
+                ans.append(path[:])
+                return
+            # 选定列
+            for col in range(n):
+                if dp[row][col] != 0: continue
+                path.append(col)
+                # 选定某一列
+                # 需要标记整个棋盘
+                tag(row, col)
+                dfs(row + 1)
+                untag(row, col)
+                path.pop()
+
+        def getans(ans):
+            result = []
+            for row in ans:  # 每个 row 是一个解，如 [0,1] 或 [1,0]
+                board = []
+                for col_index in row:
+                    s = '.' * col_index + 'Q' + '.' * (n - col_index - 1)
+                    board.append(s)
+                result.append(board)
+            return result
+
+        dfs(0)
+        # print(ans)
+        return getans(ans)
+
+
+# 37. 解数独
+topic="""
+    编写一个程序，通过填充空格来解决数独问题。
+
+数独的解法需 遵循如下规则：
+
+数字 1-9 在每一行只能出现一次。
+数字 1-9 在每一列只能出现一次。
+数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+数独部分空格内已填入了数字，空白格用 '.' 表示。
+
+输入：board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+输出：[["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+解释：输入的数独如上图所示，唯一有效的解决方案如下所示：
+
+提示：
+
+board.length == 9
+board[i].length == 9
+board[i][j] 是一位数字或者 '.'
+题目数据 保证 输入数独仅有一个解
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
