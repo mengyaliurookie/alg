0f4fc918a8b58127113e4e29ef87fc7a5c3c5392
lmy <mengyaliu6013@163.com>
2025-09-19 16:45:36 +0800

    day46 动态规划完结以及部分单调栈问题

--- 代码随想录刷题/动态规划/day46.py
+++ 代码随想录刷题/动态规划/day46.py
@@ -130,14 +130,137 @@ topic="""
 输出：2
 解释：一个可能的最长回文子序列为 "bb" 。
 """
+# 记忆化搜索的方式
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # 回文字符串，如果把字符串反转一下，是和原来的是相等的
+        # 所以可以转化为求两个字符串的最长公共子序列
+        # 算了，先试一下直接用递归的方式求解吧
+        # 从头和尾开始可以选或者不选，当i，j指向的字符相等的时候，就加2
+        # 不相等，就选择分别选i，j两个不同的字符的时候的大的子问题
+        n=len(s)
+        @cache
+        def dfs(i,j):
+            # print(f"i: {i}")
+            # print(f"j: {j}")
+            if i==j:
+                return 1
+            if i>j:
+                return 0
+            return dfs(i+1,j-1)+2 if s[i]==s[j] else max(dfs(i+1,j),dfs(i,j-1))
+        return dfs(0,n-1)
+
+# 动态规划的求解形式
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # 回文字符串，如果把字符串反转一下，是和原来的是相等的
+        # 所以可以转化为求两个字符串的最长公共子序列
+        # 算了，先试一下直接用递归的方式求解吧
+        # 从头和尾开始可以选或者不选，当i，j指向的字符相等的时候，就加2
+        # 不相等，就选择分别选i，j两个不同的字符的时候的大的子问题
+        n=len(s)
+        # 改成动态规划的形式，进行求解
+        # dp[i][j]中i和j表示在s[i:j+1]中间的最大回文子串长度
+        # 递推公式是
+        dp=[[0]*n for _ in range(n)]
+        # 初始化
+        for i in range(n):
+            dp[i][i]=1
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                
+                    if s[i]==s[j]:
+                        dp[i][j]=dp[i+1][j-1]+2
+                    else:
+                        dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+        # print(dp)
+        return dp[0][-1]
 
 
+# 739. 每日温度
+topic="""
+给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
 
+ 
 
+示例 1:
 
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出: [1,1,4,2,1,1,0,0]
+示例 2:
 
+输入: temperatures = [30,40,50,60]
+输出: [1,1,1,0]
+示例 3:
 
+输入: temperatures = [30,60,90]
+输出: [1,1,0]
+"""
+# 单调栈的实现
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # 先初始化结果集合
+        ans=[0]*len(temperatures)
+        # 需要一个数据结构来保存还没有找到下一个更高温度的ind
+        # 先入的要后出，所以需要栈
+        q=deque()
+        # 同时要去除可以直接判断的
+        for i,v in enumerate(temperatures):
+            # 如果队列为空，或者队列的头部元素大于当前元素，就直接入队
+            # print(q)
+            if not q or temperatures[q[-1]]>=v:
+                q.append(i)
+            else:
+                while q and temperatures[q[-1]]<v:
+                    ind=q.pop()
+                    ans[ind]=i-ind
+                q.append(i)
+        return ans
 
+# 496.下一个更大元素 I
+topic="""
+nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
 
+给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+
+对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+
+返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+
+ 
+
+示例 1：
+
+输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+输出：[-1,3,-1]
+解释：nums1 中每个值的下一个更大元素如下所述：
+- 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+- 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+- 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+示例 2：
+
+输入：nums1 = [2,4], nums2 = [1,2,3,4].
+输出：[3,-1]
+解释：nums1 中每个值的下一个更大元素如下所述：
+- 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+- 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+"""
+# 也是用单调栈的方式，先把整个nums2上的元素的下一个更大元素都求出来，然后存储到一个字典中
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # 可以利用上一道题的解法
+        # 把nums2中的所有元素的得下一个最大元素都存储起来，然后查表
+        dic=defaultdict(int)
+        stack=[]
+        for i in range(len(nums2)):
+            while stack and stack[-1]<nums2[i]:
+                v=stack.pop()
+                dic[v]=nums2[i]
+            stack.append(nums2[i])
+        ans=[-1]*len(nums1)
+        for i in range(len(nums1)):
+            if nums1[i] in dic:
+                ans[i]=dic[nums1[i]]
+        return ans
 
 
